f848f25fe0c8352397d8e3a8851e77cad684cfd0
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-02-07 23:59:00 -0300

    edicao pagina inicial

--- my-website/src/components/HomepageFeatures/index.js
+++ my-website/src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Projetos',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Veja os projetos que eu desenvolvi.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Experiências',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Confira as experiências que eu tive ao longo da minha carreira.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Certificações',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Certificações.
       </>
     ),
   },
