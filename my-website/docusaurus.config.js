f9ee5803544050cdb04c0f1676af0154a0db6576
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-02-08 11:08:44 -0300

    paginas de blogs desativadas

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -46,13 +46,14 @@ const config = {
           editUrl:
             'https://github.com/Rodrigo-07/DocusaurusRodrigoSales',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
+        // {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
         theme: {
           customCss: './src/css/custom.css',
         },
